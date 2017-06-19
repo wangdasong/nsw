7989ea21cf96002c1ae046ab979f9c7d1d319405
wangdasong <wang_ds@cpscs.cc>
2017-06-19 16:52:34 +0800

    1、对象转换Map功能增加迭代父类属性

--- nsw-base-core/src/main/java/nsw/base/core/utils/ConvertObject.java
+++ nsw-base-core/src/main/java/nsw/base/core/utils/ConvertObject.java
@@ -40,30 +40,61 @@ public class ConvertObject {
 					//System.out.println("methodName["+methodName+"] is not fond");
 				}
 			}
-			if(obj[i].getClass().getSuperclass() != null){
-				String superClassName = obj[i].getClass().getSuperclass().getName();
-				Class superClazzModel = Class.forName(superClassName);
-				Field superFields[] = superClazzModel.getDeclaredFields();
-				for (int j = 0; j < superFields.length; j++ ){
-					String fieldName = superFields[j].getName();
-					if(fieldNameList.contains(fieldName)){
-						continue;
+			superClassToMap(resultMap, fieldNameList, obj.length, shortClassName, obj[i].getClass(), obj[i]);
+//			if(obj[i].getClass().getSuperclass() != null){
+//				String superClassName = obj[i].getClass().getSuperclass().getName();
+//				Class superClazzModel = Class.forName(superClassName);
+//				Field superFields[] = superClazzModel.getDeclaredFields();
+//				for (int j = 0; j < superFields.length; j++ ){
+//					String fieldName = superFields[j].getName();
+//					if(fieldNameList.contains(fieldName)){
+//						continue;
+//					}
+//					String headChar = fieldName.substring(0,1);
+//					headChar = headChar.toUpperCase();
+//					String methodName = "get" + headChar + fieldName.substring(1);
+//					try{
+//						Object fieldValue = superClazzModel.getMethod(methodName).invoke(obj[i]);
+//						if(obj.length == 1 ){
+//							resultMap.put(fieldName, fieldValue);
+//						}else{
+//							resultMap.put(shortClassName+"_"+fieldName, fieldValue);
+//						}
+//					}catch(Exception e){
+//						//System.out.println("methodName["+methodName+"] is not fond");
+//					}
+//				}
+//			}
+		}
+		return resultMap;
+	}
+	private static Map<String, Object> superClassToMap(Map<String, Object> resultMap, List fieldNameList, int ObjCount, String shortClassName, Class clazz, Object obj) throws ClassNotFoundException{
+		if(clazz.getSuperclass() != null){
+			String superClassName = clazz.getSuperclass().getName();
+			Class superClazzModel = Class.forName(superClassName);
+			Field superFields[] = superClazzModel.getDeclaredFields();
+			for (int j = 0; j < superFields.length; j++ ){
+				String fieldName = superFields[j].getName();
+				if(fieldNameList.contains(fieldName)){
+					continue;
+				}
+				String headChar = fieldName.substring(0,1);
+				headChar = headChar.toUpperCase();
+				String methodName = "get" + headChar + fieldName.substring(1);
+				try{
+					Object fieldValue = superClazzModel.getMethod(methodName).invoke(obj);
+					if(ObjCount == 1 ){
+						resultMap.put(fieldName, fieldValue);
+					}else{
+						resultMap.put(shortClassName+"_"+fieldName, fieldValue);
 					}
-					String headChar = fieldName.substring(0,1);
-					headChar = headChar.toUpperCase();
-					String methodName = "get" + headChar + fieldName.substring(1);
-					try{
-						Object fieldValue = superClazzModel.getMethod(methodName).invoke(obj[i]);
-						if(obj.length == 1 ){
-							resultMap.put(fieldName, fieldValue);
-						}else{
-							resultMap.put(shortClassName+"_"+fieldName, fieldValue);
-						}
-					}catch(Exception e){
-						//System.out.println("methodName["+methodName+"] is not fond");
-					}
+				}catch(Exception e){
+					//System.out.println("methodName["+methodName+"] is not fond");
 				}
 			}
+		}
+		if(clazz.getSuperclass().getSuperclass() != null){
+			superClassToMap(resultMap, fieldNameList, ObjCount, shortClassName, clazz.getSuperclass(), obj);
 		}
 		return resultMap;
 	}
