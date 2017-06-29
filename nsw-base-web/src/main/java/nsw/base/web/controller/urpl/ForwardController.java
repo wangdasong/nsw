fe5c8a8214d105a6c3b70cb07a0e2bf6a62c6193
wangdasong <wang_ds@cpscs.cc>
2017-06-29 11:08:56 +0800

    修改错误页面不能返回的BUG

--- nsw-base-web/src/main/java/nsw/base/web/controller/urpl/ForwardController.java
+++ nsw-base-web/src/main/java/nsw/base/web/controller/urpl/ForwardController.java
@@ -1,7 +1,15 @@
 package nsw.base.web.controller.urpl;
 
+import javax.servlet.http.HttpServletRequest;
+
+import nsw.base.core.utils.ShiroFilterUtils;
+import nsw.base.core.utils.result.ResultString;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sun.corba.se.spi.orbutil.fsm.Guard.Result;
 
 /**
  * 视图控制�?,返回视图给前�?
@@ -9,7 +17,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/forward")
 public class ForwardController {
-	
+
+	/**
+     * ajax请求返回json
+     */
+    @RequestMapping("/errorJson")
+    @ResponseBody
+    public ResultString errorJson(HttpServletRequest request, Integer errorType) {
+    	ResultString resultString = new ResultString();
+    	resultString.setStatus(errorType);
+    	resultString.setMsg("");
+    	resultString.setData(null);
+        return resultString;
+    }
+    
 	/**
      * 首页
      */
@@ -31,7 +52,10 @@ public class ForwardController {
      * 404�?
      */
     @RequestMapping("/404")
-    public String error404() {
+    public String error404(HttpServletRequest request) {
+    	if(ShiroFilterUtils.isAjax(request)){
+    		return "forword:/rest/forward/errorJson?errorType=404";
+    	}
         return "views/errorpage/404";
     }
     
@@ -41,7 +65,10 @@ public class ForwardController {
      * 404的备用页面，风格略有不同
      */
     @RequestMapping("/4042")
-    public String error4042() {
+    public String error4042(HttpServletRequest request) {
+    	if(ShiroFilterUtils.isAjax(request)){
+    		return "forword:/rest/forward/errorJson?errorType=4042";
+    	}
         return "views/errorpage/404_2";
     }
 
@@ -50,7 +77,10 @@ public class ForwardController {
      * 401�?
      */
     @RequestMapping("/401")
-    public String error401() {
+    public String error401(HttpServletRequest request) {
+    	if(ShiroFilterUtils.isAjax(request)){
+    		return "forword:/rest/forward/errorJson?errorType=401";
+    	}
         return "views/errorpage/401";
     }
     /**
@@ -61,7 +91,10 @@ public class ForwardController {
      * 可以当做401页面的备用页面使用�??
      */
     @RequestMapping("/4012")
-    public String error4012() {
+    public String error4012(HttpServletRequest request) {
+    	if(ShiroFilterUtils.isAjax(request)){
+    		return "forword:/rest/forward/errorJson?errorType=4012";
+    	}
     	return "views/errorpage/401_2";
     }
 
@@ -69,7 +102,10 @@ public class ForwardController {
      * 500�?
      */
     @RequestMapping("/500")
-    public String error500() {
+    public String error500(HttpServletRequest request) {
+    	if(ShiroFilterUtils.isAjax(request)){
+    		return "forward:/rest/forward/errorJson?errorType=500";
+    	}
         return "views/errorpage/500";
     }
 
