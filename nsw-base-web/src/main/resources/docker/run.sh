41db766169a5be043b803c72c7f0b388bebf5361
wangdasong <wangds@cpscs.cc>
2017-10-24 18:10:00 +0800

    1、修改配置文件 2、修改Docker run文件

--- nsw-base-web/src/main/resources/docker/run.sh
+++ nsw-base-web/src/main/resources/docker/run.sh
@@ -3,7 +3,28 @@
 rm -rf /var/lib/mysql/*
 mysql_install_db --user=mysql --ldata=/var/lib/mysql
 #启动mysql服务
-mysqld_safe &
+echo "#############################开始启动mysql#########################"
+mysqld --user=mysql --datadir=/var/lib/mysql --skip-networking &
+pid="$!"
+mysql=( mysql --protocol=socket -uroot )
+for i in {30..0}; do
+    if echo 'SELECT 1' | "${mysql[@]}" &> /dev/null; then
+        break
+    fi
+    echo 'MySQL init process in progress...'
+    sleep 1
+done
+if [ "$i" = 0 ]; then
+    echo >&2 'MySQL init process failed.'
+    exit 1
+fi
+echo "#############################结束启动mysql#########################"
+
+#启动redis
+echo "#############################开始启动redis#########################"
+service redis start
+echo "#############################结束启动redis#########################"
+
 #修改数据库root用户的密码
 mysql -u root << !
 UPDATE mysql.user SET Password = PASSWORD('123456') WHERE User = 'root';
@@ -19,11 +40,9 @@ DBNAME="framework"                                                       #数据
 
 #创建数据库
 create_db_sql="create database IF NOT EXISTS ${DBNAME}"
-mysql -h${HOSTNAME}  -P${PORT}  -u${USERNAME} -p${PASSWORD} -e "${create_db_sql}"
+mysql -u${USERNAME} -p${PASSWORD} -e "${create_db_sql}"
 #导入数据
-mysql -h${HOSTNAME}  -P${PORT}  -u${USERNAME} -p${PASSWORD} -D ${DBNAME} -e "/opt/src-nsw/nsw-base-web/src/main/resources/docker/framework.sql"
+mysql -u${USERNAME} -p${PASSWORD} -D ${DBNAME} < /opt/src-nsw/nsw-base-web/src/main/resources/docker/framework.sql
 
-#启动redis
-service redis start
 /opt/tomcat7/bin/startup.sh
 tail -f /opt/tomcat7/logs/catalina.out
